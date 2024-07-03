cd94ac46802ce9f41aa09cee29f2802ed1340745
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-07-03 17:07:29 +0600

    add: descriptions lang json to inbound sales dictionary{

--- src/app/[lang]/inbound-sales/page.jsx
+++ src/app/[lang]/inbound-sales/page.jsx
@@ -1,11 +1,14 @@
-import { Hero } from '@/components/pages/homepage'
-import { getInboundSalesDict } from '@/utils/i18n/get-dictionary'
+import { AskAgent, Clients, Hero } from '@/components/pages/homepage'
+import { getHomepageDict, getInboundSalesDict } from '@/utils/i18n/get-dictionary'
 
 export default async function InboundSalesPage({ params: { lang } }) {
   const t = await getInboundSalesDict(lang)
+  const homeT = await getHomepageDict(lang)
   return (
     <main>
+      <AskAgent t={homeT.askAgent} />
       <Hero t={t.hero} />
+      <Clients t={homeT.clients} />
     </main>
   )
 }
