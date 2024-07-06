7e4c26a29f15200b0b6a0791059cf660d28862f0
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-07-07 05:43:59 +0600

    fix: dict not found in layout

--- src/app/[lang]/(rootLayout)/layout.jsx
+++ src/app/[lang]/(rootLayout)/layout.jsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/common/Footer'
 import Navbar from '@/components/common/Navbar'
+import { getCommonDict } from '@/utils/i18n/get-dictionary'
 
 export default async function Layout({ children, params: { lang } }) {
   const t = await getCommonDict(lang)
--- src/app/[lang]/signup/page.jsx
+++ src/app/[lang]/signup/page.jsx
@@ -1,3 +1,21 @@
+//import bg from '@/assets/images/pages/auth/signup-hero.png'
+import { Input } from '@/components/ui/input'
+import Typography from '@/components/ui/typography'
+
 export default function SignupPage() {
-  return <div>SignupPage</div>
+  return (
+    <main className='p-10 flex min-h-screen items-center'>
+      <div className='w-1/2 h-screen' style={{ backgroundImage: `url(/signup-hero.png)` }}>
+        {/* <Img src={bg} alt='Inova signup' className='w-full h-auto object-cover' /> */}
+      </div>
+      <div className='w-1/2 flex flex-col items-center justify-center text-center text-balance'>
+        <Typography variant='h3'>Welcome to Inova!</Typography>
+        <p className='text-text-secondary font-medium'>Register now and start your adventure.</p>
+
+        <Input />
+        <Input />
+        <Input />
+      </div>
+    </main>
+  )
 }
