c9363e7e2872524c1f4684d7f2bec4a5b2a46791
Naimur <naeemhasan28@gmail.com>
2024-07-08 15:46:12 +0600

    add: descriptions sections in inbound sales page

--- src/app/[lang]/(rootLayout)/inbound-sales/page.jsx
+++ src/app/[lang]/(rootLayout)/inbound-sales/page.jsx
@@ -1,4 +1,4 @@
-import { AskAgent, Clients, Hero } from '@/components/pages/homepage'
+import { AskAgent, Clients, Description1, Description2, Description3, Hero } from '@/components/pages/homepage'
 import { getHomepageDict, getInboundSalesDict } from '@/utils/i18n/get-dictionary'
 
 export default async function InboundSalesPage({ params: { lang } }) {
@@ -9,6 +9,11 @@ export default async function InboundSalesPage({ params: { lang } }) {
       <AskAgent t={homeT.askAgent} />
       <Hero t={t.hero} />
       <Clients t={homeT.clients} />
+      <Description1 t={t.description1} />
+      <Description2 t={t.description2} />
+      <Description3 t={t.description3} />
+      <Description2 t={t.description4} />
+      <Description3 t={t.description5} />
     </main>
   )
 }
