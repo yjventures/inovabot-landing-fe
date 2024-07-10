82e854fbeeaf9462e0a6ed919726e629768d4a53
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-07-10 11:05:41 +0600

    add: case study header component

--- src/app/[lang]/(rootLayout)/case-study/page.jsx
+++ src/app/[lang]/(rootLayout)/case-study/page.jsx
@@ -1,3 +1,15 @@
-export default function CaseStudyPage() {
-  return <div>CaseStudyPage</div>
+import CaseStudyHeader from '@/components/pages/case-study/CaseStudyHeader'
+import { getCaseStudyDict } from '@/utils/i18n/get-dictionary'
+
+export const metadata = {
+  title: 'Case Study | Inova'
+}
+
+export default async function CaseStudyPage({ params: { lang } }) {
+  const t = await getCaseStudyDict(lang)
+  return (
+    <main>
+      <CaseStudyHeader t={t.header} />
+    </main>
+  )
 }
---
+++ src/components/pages/case-study/CaseStudyHeader.jsx
@@ -0,0 +1,30 @@
+import TagLine from '@/components/common/TagLine'
+import Typography from '@/components/ui/typography'
+
+export default function CaseStudyHeader({ t }) {
+  return (
+    <section className='bg-primary-foreground -mt-20 py-20 md:py-32'>
+      <div className='container flex flex-col items-center justify-center'>
+        <TagLine className='uppercase'>{t.tag}</TagLine>
+        <Typography
+          variant='h2'
+          className='text-balance text-center pt-10 pb-6 leading-loose text-2xl md:text-3xl lg:text-5xl max-w-full'
+        >
+          {t.title}
+        </Typography>
+        <p className='text-text-tartiary text-center max-w-full font-medium text-lg pb-10'>{t.description}</p>
+        <div className='rounded-2xl max-w-[950px] w-full aspect-video'>
+          <iframe
+            src='https://www.youtube.com/embed/k85mRPqvMbE?si=Sm5m7JmCudUXNUhE'
+            title='YouTube video player'
+            frameborder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            referrerpolicy='strict-origin-when-cross-origin'
+            allowfullscreen
+            className='w-full h-full rounded-2xl z-10 relative'
+          ></iframe>
+        </div>
+      </div>
+    </section>
+  )
+}
