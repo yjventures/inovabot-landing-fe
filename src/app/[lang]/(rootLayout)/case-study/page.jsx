1da1c92b610a7cb9bcd70b1a143c5c569bb2f88f
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-07-10 12:00:04 +0600

    add: case study get started cta and testimonials section

--- src/app/[lang]/(rootLayout)/case-study/page.jsx
+++ src/app/[lang]/(rootLayout)/case-study/page.jsx
@@ -2,7 +2,9 @@ import CaseStudyAbout from '@/components/pages/case-study/CaseStudyAbout'
 import CaseStudyHeader from '@/components/pages/case-study/CaseStudyHeader'
 import Results from '@/components/pages/case-study/Results'
 import WhatWeDid from '@/components/pages/case-study/WhatWeDid'
-import { getCaseStudyDict } from '@/utils/i18n/get-dictionary'
+import GetStartedCTA from '@/components/pages/contact-support/GetStartedCTA'
+import { Testimonials } from '@/components/pages/homepage'
+import { getCaseStudyDict, getHomepageDict } from '@/utils/i18n/get-dictionary'
 
 export const metadata = {
   title: 'Case Study | Inova'
@@ -10,12 +12,15 @@ export const metadata = {
 
 export default async function CaseStudyPage({ params: { lang } }) {
   const t = await getCaseStudyDict(lang)
+  const homeT = await getHomepageDict(lang)
   return (
     <main>
       <CaseStudyHeader t={t.header} />
       <CaseStudyAbout t={t.about} />
       <WhatWeDid t={t.whatWeDid} />
       <Results t={t.results} />
+      <GetStartedCTA t={t.getStartedCTA} className='pt-10 pb-20 md:pb-32' />
+      <Testimonials t={homeT.testimonials} />
     </main>
   )
 }
