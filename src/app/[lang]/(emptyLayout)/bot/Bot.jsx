cc3278402efb625acca02df1322a3f999e6ac598
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-07-28 12:11:30 +0600

    refactor functions and modify textarea and input component

--- src/app/[lang]/(emptyLayout)/bot/Bot.jsx
+++ src/app/[lang]/(emptyLayout)/bot/Bot.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/no-children-prop */
 'use client'
 
-import { Input } from '@/components/ui/input'
 import MarkdownRenderer from '@/components/ui/markdown-renderer'
 import { Skeleton } from '@/components/ui/skeleton'
 import botData from '@/constants/bot-page-temp.json'
@@ -16,6 +15,7 @@ import rigmtImg from '@/assets/temp/right-img.png'
 import lightBg from '@/assets/temp/violet-bg.jpg'
 import Spinner from '@/components/icons/Spinner'
 import { Img } from '@/components/ui/img'
+import { Textarea } from '@/components/ui/textarea'
 import { AlignRight, PlayCircle, StopCircle } from 'lucide-react'
 import AudioRecorder from './AudioRecorder'
 import { faqs, fetchData } from './BotContainer'
@@ -81,6 +81,39 @@ export default function Bot({
     }
   }
 
+  const handleFAQTrigger = () => {
+    setMessage(faq)
+    fetchData({
+      msg: faq,
+      setisLoading,
+      setTempMessages,
+      tempMessages,
+      id,
+      cb: () => {
+        refetch()
+        setisPlaying(false)
+      },
+      setMessage,
+      setaudioURL
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (message === '') return
+
+    fetchData({
+      msg: message,
+      setisLoading,
+      setTempMessages,
+      tempMessages,
+      id,
+      cb: refetch,
+      setMessage,
+      setaudioURL
+    })
+  }
+
   return (
     <main className='relative h-screen'>
       {isLoading ? (
@@ -116,26 +149,7 @@ export default function Bot({
           }}
         >
           {faqs.map(faq => (
-            <p
-              key={faq}
-              className='font-medium cursor-pointer'
-              onClick={() => {
-                setMessage(faq)
-                fetchData({
-                  msg: faq,
-                  setisLoading,
-                  setTempMessages,
-                  tempMessages,
-                  id,
-                  cb: () => {
-                    refetch()
-                    setisPlaying(false)
-                  },
-                  setMessage,
-                  setaudioURL
-                })
-              }}
-            >
+            <p key={faq} className='font-medium cursor-pointer' onClick={handleFAQTrigger}>
               {faq}
             </p>
           ))}
@@ -210,43 +224,33 @@ export default function Bot({
         <Img src={rigmtImg} alt='right image' className='w-80 hidden xl:block h-auto self-start mt-20' />
       </div>
       <div className='fixed bottom-5 left-1/2 -translate-x-1/2 max-w-4xl w-full px-5 flex items-center gap-x-3'>
-        <div className='flex items-center justify-between rounded-xl gap-x-3 bg-white w-full'>
-          <Input
+        <form onSubmit={handleSubmit} className='flex items-center justify-between rounded-xl gap-x-3 bg-white w-full'>
+          <Textarea
             type='text'
-            containerClassName='w-full max-w-full h-14'
-            className='w-full max-w-full h-14 border-none focus-visible:ring-0'
+            containerClassName='w-full max-w-full min-h-14'
+            className='w-full max-w-full min-h-14 border-none focus-visible:ring-0'
             placeholder='Type a message...'
             value={message}
             onChange={e => setMessage(e.target.value)}
           />
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 64 64'
-            id='send'
-            className='size-8 cursor-pointer text-violet-800 mr-2 fill-current'
-            onClick={() =>
-              fetchData({
-                msg: message,
-                setisLoading,
-                setTempMessages,
-                tempMessages,
-                id,
-                cb: refetch,
-                setMessage,
-                setaudioURL
-              })
-            }
-          >
-            <defs>
-              <clipPath id='a'>
-                <rect width='64' height='64'></rect>
-              </clipPath>
-            </defs>
-            <g clip-path='url(#a)'>
-              <path d=' M 8.216 36.338 L 26.885 32.604 C 28.552 32.271 28.552 31.729 26.885 31.396 L 8.216 27.662 C 7.104 27.44 6.021 26.356 5.799 25.245 L 2.065 6.576 C 1.731 4.908 2.714 4.133 4.259 4.846 L 61.228 31.139 C 62.257 31.614 62.257 32.386 61.228 32.861 L 4.259 59.154 C 2.714 59.867 1.731 59.092 2.065 57.424 L 5.799 38.755 C 6.021 37.644 7.104 36.56 8.216 36.338 Z '></path>
-            </g>
-          </svg>
-        </div>
+          <button type='submit'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 64 64'
+              id='send'
+              className='size-8 cursor-pointer text-violet-800 mr-2 fill-current'
+            >
+              <defs>
+                <clipPath id='a'>
+                  <rect width='64' height='64'></rect>
+                </clipPath>
+              </defs>
+              <g clip-path='url(#a)'>
+                <path d=' M 8.216 36.338 L 26.885 32.604 C 28.552 32.271 28.552 31.729 26.885 31.396 L 8.216 27.662 C 7.104 27.44 6.021 26.356 5.799 25.245 L 2.065 6.576 C 1.731 4.908 2.714 4.133 4.259 4.846 L 61.228 31.139 C 62.257 31.614 62.257 32.386 61.228 32.861 L 4.259 59.154 C 2.714 59.867 1.731 59.092 2.065 57.424 L 5.799 38.755 C 6.021 37.644 7.104 36.56 8.216 36.338 Z '></path>
+              </g>
+            </svg>
+          </button>
+        </form>
         <AudioRecorder
           id={id}
           message={message}
--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -34,54 +34,52 @@ const Input = forwardRef(
             {required ? '*' : null}
           </Label>
         )}
-        <div className=''>
-          <div className='relative'>
-            <input
-              type={type === 'password' ? (showPassword ? 'text' : 'password') : type}
-              className={cn(
-                'flex h-10 w-full rounded-lg border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary disabled:cursor-not-allowed disabled:opacity-50',
-                { 'pl-10': icon },
-                className
+        <div className='relative'>
+          <input
+            type={type === 'password' ? (showPassword ? 'text' : 'password') : type}
+            className={cn(
+              'flex h-10 w-full rounded-lg border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary disabled:cursor-not-allowed disabled:opacity-50',
+              { 'pl-10': icon },
+              className
+            )}
+            ref={ref}
+            {...inputProps}
+          />
+          {type === 'password' ? (
+            <>
+              {showPassword ? (
+                <Eye
+                  className='absolute right-3 top-1/2 -translate-y-1/2 size-5 cursor-pointer text-text'
+                  onClick={() => setshowPassword(false)}
+                />
+              ) : (
+                <EyeOff
+                  className='absolute right-3 top-1/2 -translate-y-1/2 size-5 cursor-pointer text-text'
+                  onClick={() => setshowPassword(true)}
+                />
               )}
-              ref={ref}
-              {...inputProps}
-            />
-            {type === 'password' ? (
-              <>
-                {showPassword ? (
-                  <Eye
-                    className='absolute right-3 top-1/2 -translate-y-1/2 size-5 cursor-pointer text-text'
-                    onClick={() => setshowPassword(false)}
-                  />
-                ) : (
-                  <EyeOff
-                    className='absolute right-3 top-1/2 -translate-y-1/2 size-5 cursor-pointer text-text'
-                    onClick={() => setshowPassword(true)}
-                  />
-                )}
-              </>
-            ) : null}
-            {icon && (
-              <div
-                className={cn(
-                  'absolute inset-y-0 left-2.5 top-0 flex items-center [&>svg]:size-6 text-muted-foreground',
-                  {}
-                )}
-              >
-                {icon}
-              </div>
+            </>
+          ) : null}
+          {icon && (
+            <div
+              className={cn(
+                'absolute inset-y-0 left-2.5 top-0 flex items-center [&>svg]:size-6 text-muted-foreground',
+                {}
+              )}
+            >
+              {icon}
+            </div>
+          )}
+        </div>
+        {required ? (
+          <div className='flex justify-start mt-2'>
+            {errors[name] && errors[name]?.type === 'required' ? (
+              <span className='text-red-500 text-xs h-5 leading-none !text-left'>{label} is required</span>
+            ) : (
+              <div className='w-full h-5' />
             )}
           </div>
-          {required ? (
-            <div className='flex justify-start mt-2'>
-              {errors[name] && errors[name]?.type === 'required' ? (
-                <span className='text-red-500 text-xs h-5 leading-none !text-left'>{label} is required</span>
-              ) : (
-                <div className='w-full h-5' />
-              )}
-            </div>
-          ) : null}
-        </div>
+        ) : null}
       </div>
     )
   }
