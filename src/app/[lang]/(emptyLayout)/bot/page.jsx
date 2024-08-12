658fe4a205aa1d13aafddc17d0215f1a3fc84e85
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-08-12 18:35:05 +0600

    modify: rename bot id

--- src/app/[lang]/(emptyLayout)/bot/page.jsx
+++ src/app/[lang]/(emptyLayout)/bot/page.jsx
@@ -4,7 +4,7 @@ export const metadata = {
   title: 'BotPage | Inova'
 }
 
-// const botId = '668fadec9577e0dfdeaf9430'
+// const threadId = '668fadec9577e0dfdeaf9430'
 const threadId = '66a4c43398f8967482e2b197'
 
 export default function BotPage() {
