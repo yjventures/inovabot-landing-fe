a85c3f3f0c1f0f569cf7202f845291831719e432
abraranwar09 <abraranwar09@gmail.com>
2024-09-06 16:45:54 +0600

    njs change for s3

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['rfqbucket.s3-website-ap-southeast-2.amazonaws.com']
+    domains: ['rfqbucket.s3-website-ap-southeast-2.amazonaws.com', 's3.ap-southeast-2.amazonaws.com']
   }
 }
 
