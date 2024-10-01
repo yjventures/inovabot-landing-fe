3c2a115803dc7dd01786d0eccfa26e8cb83f4e06
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-10-01 14:44:01 +0600

    add: error and not found pages

---
+++ src/app/error.jsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import Typography from '@/components/ui/typography'
+import { cn } from '@/lib/utils'
+import { useEffect } from 'react'
+
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className={cn('min-h-screen flex flex-col items-center justify-center gap-y-4')}>
+      <h1 className='text-9xl font-light text-destructive'>Error</h1>
+      <Typography variant='h1' className='font-semibold text-text-secondary'>
+        Something went wrong!
+      </Typography>
+      <p className='text-xl text-text-tartiary mt-2 mb-4'>{error.message}</p>
+
+      <Button variant='black' onClick={() => reset()}>
+        Try again
+      </Button>
+    </div>
+  )
+}
---
+++ src/app/not-found.jsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button'
+import Typography from '@/components/ui/typography'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+
+export const metadata = { title: '404 | Page not found' }
+
+export default function NotFound() {
+  return (
+    <div className={cn('min-h-screen flex flex-col items-center justify-center gap-y-4')}>
+      <h1 className='text-9xl font-light'>404</h1>
+      <Typography variant='h1' className='font-semibold text-text-secondary'>
+        Page not found
+      </Typography>
+      <p className='text-xl text-text-tartiary mt-2 mb-4'>
+        Sorry, we couldn&apos;t find the page you&apos;re looking for.
+      </p>
+
+      <Link href='/'>
+        <Button variant='black'>&larr; Go to home</Button>
+      </Link>
+    </div>
+  )
+}
